18b26283d28e1b38852cffa356f7fb70992d9e04
alexusljf <limjinfeng@live.com>
2023-10-01 15:03:02 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './screens/Home/Home';
 import About from './screens/About/About';
 import PortPage from './screens/Ports/PortPage';
@@ -8,17 +8,18 @@ import Contact from './screens/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/port/:portName" element={<PortPage />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+    <div className='App'>
+        <BrowserRouter>
+                <Routes>
+                  <Route path="/PSA-Code-Sprint-2023/" element={<Home/>} />
+                  <Route path="/PSA-Code-Sprint-2023/about" element={<About/>} />
+                  <Route path="/PSA-Code-Sprint-2023/port/:portName" element={<PortPage />} />
+                  <Route path="/PSA-Code-Sprint-2023/faq" element={<FAQ/>} />
+                  <Route path="/PSA-Code-Sprint-2023/contact" element={<Contact/>} />
+                </Routes>
+            </BrowserRouter>
     </div>
+    
   );
 }
 
